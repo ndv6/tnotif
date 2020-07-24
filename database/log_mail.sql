a75b6751b24f869cbc1b0e51e2e3c93e4475c157
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-07-24 11:45:05 +0700

    dockerize api and create table automatically in postgres docker

--- database/log_mail.sql
+++ database/log_mail.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXISTS "log_mail";
 DROP SEQUENCE IF EXISTS log_mail_id_seq;
 CREATE SEQUENCE log_mail_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
 
-CREATE TABLE "public"."log_mail" (
+CREATE TABLE IF NOT EXISTS "public"."log_mail" (
     id integer DEFAULT nextval('log_mail_id_seq') NOT NULL,
     email character varying(100),
     sent_at timestamp
