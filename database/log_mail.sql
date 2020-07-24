b3b7446ce38730838b551e6b2fc42851ad1a2cfa
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-07-24 12:00:49 +0700

    edit id to serial

--- database/log_mail.sql
+++ database/log_mail.sql
@@ -1,8 +1,5 @@
-DROP SEQUENCE IF EXISTS log_mail_id_seq;
-CREATE SEQUENCE log_mail_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
-
 CREATE TABLE IF NOT EXISTS "public"."log_mail" (
-    id integer DEFAULT nextval('log_mail_id_seq') NOT NULL,
+    id SERIAL NOT NULL,
     email character varying(100),
     sent_at timestamp
 )   WITH (oids = false);
