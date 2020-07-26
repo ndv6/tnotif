f414558e83026bcb07d441933b3a055f485eacee
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-07-26 10:38:31 +0700

    change response and add constants

---
+++ constants/constants.go
@@ -0,0 +1,29 @@
+package constants
+
+// Response status
+const (
+	Success = "SUCCESS"
+	Failed  = "FAILED"
+)
+
+// Error messages
+const (
+	RequestHasInvalidFields = "Mandatory field(s) is missing from request, or is invalid. Please ensure all fields are properly filled."
+	CannotReadRequest       = "Cannot read request body"
+	CannotParseRequest      = "Unable to parse request"
+	UnauthorizedRequest     = "Failed to recognize deposit source. Please use authorized banks to deposit."
+	CannotEncodeResponse    = "Failed to encode response."
+	InsertFailed            = "Failed to insert to database."
+	InitLogFailed           = "Failed to insert log of this transaction."
+	CannotParseURLParams    = "Failed to parse URL Params"
+)
+
+// Headers
+const (
+	ContentType = "Content-Type"
+)
+
+// Header types
+const (
+	JSON = "application/json"
+)
